6f5157abf77a4e063087f013dcfc547865ca6b74
Oleg Ghimp <ghimpco@gmail.com>
2022-05-22 03:40:33 +0300

    dockerized backend

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,4 @@
 import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
-import org.springframework.boot.gradle.tasks.bundling.BootJar
-import org.springframework.boot.gradle.tasks.run.BootRun
 
 plugins {
     id("org.springframework.boot") version "2.6.7"
@@ -9,7 +7,7 @@ plugins {
 }
 
 group = "com.oxyac"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1"
 
 
 java {
@@ -30,14 +28,23 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-web") {
+        exclude(group = "org.springframework.boot", module = "spring-boot-starter-tomcat")
+        exclude(group = "junit", module = "junit")
+    }
+    implementation("org.springframework.boot:spring-boot-starter-jetty")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+
+    implementation("io.sentry:sentry-logback:5.7.4")
     compileOnly("org.projectlombok:lombok")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    runtimeOnly("com.h2database:h2")
     runtimeOnly("org.postgresql:postgresql")
+
     annotationProcessor("org.projectlombok:lombok")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("com.h2database:h2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
